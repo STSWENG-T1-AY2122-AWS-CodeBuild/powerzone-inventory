66df89d1fd3b3d61085a23e4da3450c7fdf91541
Hylene Lee <hylene_jules_lee@dlsu.edu.ph>
2021-11-20 01:14:28 +0800

    git add .

---
+++ misc/populate-account.js
@@ -0,0 +1,36 @@
+/* Script for populating the database with the admin account details */
+
+/* Mongoose is used for database functions */
+const mongoose = require('mongoose');
+
+/* The db file and account schema are used to manipulate the accounts on the database */
+const db = require('../models/db.js');
+const Account = require('../models/account-schema.js');
+
+/* Bcrypt is used for password hashing */
+const bcrypt = require('bcrypt');
+
+/* Ten salt rounds are used for password hashing */
+const saltRounds = 10;
+
+/* Establish a connection to the database */
+db.connect();
+
+/* Add the admin account to the database */
+bcrypt.hash("password123", saltRounds, function (err, hash) {
+	/* Initialize customer account details */
+	let adminAccount = {
+		email: "administrator@gmail.com",
+        name: "Powerzone Admin",
+        username: "powerzoneadmin",
+        role: "Administrator",
+        password: hash
+	};
+
+	/* Insert the initialized account into the database */
+	db.insertOne(Account, adminAccount, function(flag) {	
+		if (flag) {
+			console.log("\nDatabase population complete! Press Ctrl + C to continue.");
+		}
+	});
+});
