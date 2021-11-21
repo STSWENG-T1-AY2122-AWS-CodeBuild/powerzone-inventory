4d41c634a2c175f8ba926781e7d21f75d27d11ac
Hylene Lee <hylene_jules_lee@dlsu.edu.ph>
2021-11-21 10:31:22 +0800

    Add skeleton files for log in and register controllers

---
+++ controllers/log-in-controller.js
@@ -0,0 +1,18 @@
+/* Controller for displaying the log in page */
+
+/* The db file is used to display the landing page */
+const db = require('../models/db.js');
+
+const registerController = {
+	/**
+	 * Gets the log in page
+	 * 
+	 * @param req object that contains information on the HTTP request from the client
+	 * @param res object that contains information on the HTTP response from the server 
+	 */
+	getLogIn: function(req, res) {
+		
+	}
+}
+
+module.exports = indexController;
--- routes/routes.js
+++ routes/routes.js
@@ -6,14 +6,21 @@ const dotenv = require('dotenv');
 /* Use the express web application framework*/
 const express = require('express');
 
-/* Use the krafts database and the db file for accessing the database */
+/* Use the powerzone database and the db file for accessing the database */
 const powerzone = express();
 const db = require('../models/db.js');
 
 /* Call the controllers for each of the web application features */
-
+const indexController = require('../controllers/index-controller.js');
+const registerController = require('../controllers/register-controller.js');
+const logInController = require('../controllers/log-in-controller.js');
 /* Call the validation file */
 
 dotenv.config();
+
+powerzone.get('/', logInController.getLogIn);
+
+powerzone.get('/register', registerController.getRegister);
+powerzone.get('/logIn', logInController.getLogIn);
 
 module.exports = powerzone;
