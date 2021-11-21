028600f5ec1a28aa514f748f300724fd485d4807
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-11-22 00:05:10 +0800

    Remove unnecessary commands

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const nocache = require("nocache");
 
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const mongoose = require('mongoose');
-/*const session = require('express-session');*/
-//const mongoStore = require('connect-mongo')(session);
+const session = require('express-session');
+const mongoStore = require('connect-mongo')(session);
 
 const routes = require('./routes/routes.js');
-// const helper = require('./helpers/helpers.js');
 const db = require('./models/db.js');
 
 const powerzone = express();
@@ -25,24 +23,19 @@ db.connect();
 powerzone.set('view engine','hbs');
 powerzone.use(express.static(path.join(__dirname, '/public')));
 powerzone.use(express.json());
-powerzone.use(express.urlencoded({extended:true}));
-// powerzone.engine('hbs',exphbs({
-// 	defaultLayout: 'main',
-// 	extname:'.hbs',
-// 	helpers: helper})
-//     })
-//);
+powerzone.use(express.urlencoded({
+	extended:true
+}));
 
 powerzone.use(nocache());
 
-/*
 powerzone.use(session({
 	secret: process.env.session_secret,
 	resave: false,
 	saveUninitialized: false,
-//	store: new mongoStore({mongooseConnection: mongoose.connection})
+	store: new mongoStore({mongooseConnection: mongoose.connection})
 }));
-*/
+
 powerzone.use('/',routes);
 
 powerzone.listen(port, hostname, function() {
