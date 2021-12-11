b8e089d3b9b408bdf3dbbe91a3147a06d915c46e
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-12-12 03:45:02 +0800

    Finish client-side validation for register

--- public/js/register-validate.js
+++ public/js/register-validate.js
@@ -24,18 +24,22 @@ $(function(){
         const emailUntrimmed = emailField.val();
         const email = emailField.val().trim();
 
-        $.get('/getCheckUsername', {username: username}, function(res) {
-            if (res.username != username) {
-                if (field.is(usernameField)) {
-                    hideErrorMessage(nonUniqueUsername);
-                    isUsernameStillValid = true;
+        if (emailUntrimmed.length == 0) {
+            hideErrorMessage(nonUniqueEmail);
+        }
+
+        $.get('/getCheckEmail', {email: email}, function(res) {
+            if (res.email != email) {
+                if (field.is(emailField)) {
+                    hideErrorMessage(nonUniqueEmail);
+                    isEmailStillValid = true;
 
                     return callback(true);
                 }
             } else {
-                if (field.is(usernameField)) {
-                    displayErrorMessage(nonUniqueUsername);
-                    isUsernameStillValid = false;
+                if (field.is(emailField)) {
+                    displayErrorMessage(nonUniqueEmail);
+                    isEmailStillValid = false;
 
                     return callback(false);
                 }
@@ -115,34 +119,116 @@ $(function(){
         return false;
     }
 
-    function validateField(field) {
+    function validateWithEmail(field, isEmailValid) {
         if (didUsernameChange) {
             didUsernameChange = false;
 
             if (!isUsernameStillValid) {                       
                 isUsernameValid(field, function (isUsernameValid) {
-					if (isPasswordValid(field) && isUsernameValid) {
-						$('#signup-btn').prop('disabled', false);
+					if (isPasswordValid(field) && isUsernameValid && isEmailValid) {
+						enableButton($('#signup-btn'));
 					} else {	
 						$('#signup-btn').prop('disabled', true);
 					}
 				});
             } else {
-                if (isPasswordValid(field)) {	
-					$('#signup-btn').prop('disabled', false);
+                if (isPasswordValid(field) && isEmailValid) {	
+					enableButton($('#signup-btn'));
 				} else {	
 					$('#signup-btn').prop('disabled', true);
 				}
             }
 
         } else {
-            if(isPasswordValid(field) && isUsernameStillValid) {    
-                $('#signup-btn').prop('disabled', false);
+            if (isPasswordValid(field) && isUsernameStillValid && isEmailValid) {    
+                enableButton($('#signup-btn'));
             } else {    
-                $('#signup-btn').prop('disabled', true);
+               disableButton($('#signup-btn'));
             }
         }
     }
+
+    function validateWithGlobalEmailVar(field) {
+        if (didUsernameChange) {
+            didUsernameChange = false;
+
+            if (!isUsernameStillValid) {                       
+                isUsernameValid(field, function (isUsernameValid) {
+					if (isPasswordValid(field) && isUsernameValid && isEmailStillValid) {
+						enableButton($('#signup-btn'));
+					} else {	
+						$('#signup-btn').prop('disabled', true);
+					}
+				});
+            } else {
+                if (isPasswordValid(field) && isEmailStillValid) {	
+					enableButton($('#signup-btn'));
+				} else {	
+					$('#signup-btn').prop('disabled', true);
+				}
+            }
+
+        } else {
+            if (isPasswordValid(field) && isUsernameStillValid && isEmailStillValid) {    
+                enableButton($('#signup-btn'));
+            } else {    
+               disableButton($('#signup-btn'));
+            }
+        }
+    }
+
+    function validateWithoutEmail(field) {
+        if (didUsernameChange) {
+            didUsernameChange = false;
+
+            if (!isUsernameStillValid) {                       
+                isUsernameValid(field, function (isUsernameValid) {
+					if (isPasswordValid(field) && isUsernameValid) {
+						enableButton($('#signup-btn'));
+					} else {	
+						$('#signup-btn').prop('disabled', true);
+					}
+				});
+            } else {
+                if (isPasswordValid(field)) {	
+					enableButton($('#signup-btn'));
+				} else {	
+					$('#signup-btn').prop('disabled', true);
+				}
+            }
+
+        } else {
+            if (isPasswordValid(field) && isUsernameStillValid && isEmailValid) {    
+                enableButton($('#signup-btn'));
+            } else {    
+               disableButton($('#signup-btn'));
+            }
+        }
+    }
+
+    function validateField(field) {
+        if (didEmailChange) {
+            didEmailChange = false;
+
+            if (!didEmailChange) {
+                isEmailValid(field, function(isEmailValid) {
+                    validateWithEmail(field, isEmailValid);
+                });
+            } else {
+                validateWithoutEmail(field);
+            }
+
+        } else {
+            validateWithGlobalEmailVar(field);
+        }
+    }
+
+    $('#signup-email').on('keyup', function() {
+        isEmailStillValid = false;
+        didEmailChange = true;
+
+        validateField($('#signup-email'));
+    });
 
     $('#signup-username').on('keyup', function() {
         isUsernameStillValid = false;
