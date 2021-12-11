b29c67691e065c15a19e8ec90f34e33badd7e79e
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-12-11 23:09:47 +0800

    Unit test home controller

---
+++ test/home-controller-test.js
@@ -0,0 +1,18 @@
+const assert = require('chai').assert;
+const sinon = require('sinon');
+const homeController = require('../controllers/home-controller.js');
+
+describe('the function to get the home page', function() {
+    it('should render the home page', function() {
+        let req = {}
+        let res = {
+            render: sinon.spy()
+        }
+        
+        homeController.getHome(req, res);
+        console.log(res.render);
+
+        assert.isTrue(res.render.calledOnce);
+        assert.equal(res.render.firstCall.args[0], 'home');
+    });
+});
