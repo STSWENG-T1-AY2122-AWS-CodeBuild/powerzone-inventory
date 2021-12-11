7e017460f8e0113bd6dd8ce4f6f4d5289e2d5583
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-12-11 21:33:50 +0800

    Add unit tests for username and password

--- public/js/register-validate-util.js
+++ public/js/register-validate-util.js
@@ -4,6 +4,24 @@ const isPasswordLengthValid = function(password) {
     return validator.isLength(password, {min: 12});
 }
 
+const isPasswordFormatValid = function(password) {
+    return validator.matches(password, /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&^()|{}\[\]:;\"<>'`#,./~\\+=-])[A-Za-z\d@$!%*?&^()|{}\[\]:;\"<>'`#,./~\\+=-]/)
+}
+
+const isPasswordValid = function(password) {
+    return isPasswordLengthValid && isPasswordFormatValid;
+}
+
+const isUsernameLengthValid = function(username) {
+    return validator.isLength(username.trim(), {min: 1});
+}
+
+const isUsernameValid = function(password) {
+    return isUsernameLengthValid;
+}
+
 module.exports = {
-    isPasswordLengthValid
+    isPasswordLengthValid,
+    isPasswordFormatValid,
+    isUsernameLengthValid
 }
