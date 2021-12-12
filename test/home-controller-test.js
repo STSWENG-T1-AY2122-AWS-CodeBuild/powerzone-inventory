b8e7a7575657541dda2a63895749036cb142d561
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2021-12-12 16:06:55 +0800

    Finish unit testing of home controller

--- test/home-controller-test.js
+++ test/home-controller-test.js
@@ -4,16 +4,73 @@ const sinon = require('sinon');
 const homeController = require('../controllers/home-controller.js');
 
 describe('the function to get the home page', function() {
-    it('should render the home page', function() {
-        let req = {}
+    it('should render the home page only once if the user is logged in', function() {
+        let req = {
+            session: {
+                username: 'bettina'
+            }
+        };
+
         let res = {
-            render: sinon.spy()
-        }
+            render: sinon.spy(),
+            redirect: sinon.spy()
+        };
         
         homeController.getHome(req, res);
-        console.log(res.render);
 
         assert.isTrue(res.render.calledOnce);
         assert.equal(res.render.firstCall.args[0], 'home');
+    });
+
+    it('should not redirect to the log-in page if the user is logged in', function() {
+        let req = {
+            session: {
+                username: 'bettina'
+            }
+        };
+
+        let res = {
+            render: sinon.spy(),
+            redirect: sinon.spy()
+        };
+        
+        homeController.getHome(req, res);
+
+        assert.isTrue(res.redirect.notCalled);
+    });
+
+    it('should redirect to the log-in page only once if the user is not logged in', function() {
+        let req = {
+            session: {
+                username: null
+            }
+        };
+
+        let res = {
+            render: sinon.spy(),
+            redirect: sinon.spy()
+        };
+        
+        homeController.getHome(req, res);
+
+        assert.isTrue(res.redirect.calledOnce);
+        assert.equal(res.redirect.firstCall.args[0], '/');
+    });
+
+    it('should not render the home page if the user is not logged in', function() {
+        let req = {
+            session: {
+                username: null
+            }
+        };
+
+        let res = {
+            render: sinon.spy(),
+            redirect: sinon.spy()
+        };
+        
+        homeController.getHome(req, res);
+
+        assert.isTrue(res.render.notCalled);
     });
 });
