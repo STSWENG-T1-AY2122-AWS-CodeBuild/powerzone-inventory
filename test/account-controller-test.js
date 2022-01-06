8beac6d4a9b4da3fdab2420ed931642d5488f110
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2022-01-06 14:27:39 +0800

    Unit test editing of roles and deletion of account

--- test/account-controller-test.js
+++ test/account-controller-test.js
@@ -106,6 +106,143 @@ describe('the function to update the status of the selected user account to "Rej
 		expect(db.updateOne.firstCall.args[1]).to.deep.equalInAnyOrder(filter);
 		expect(db.updateOne.firstCall.args[2]).to.deep.equalInAnyOrder({status: 'Rejected'});
 
+        db.convertToObjectId.restore();
         db.updateOne.restore();
+    });
+});
+
+describe('the function to update the status of the selected user account to "Pending"', function() {
+    it('should update the status of the account to "Pending" only once', function() {
+        let convertToObjectId = sinon.stub(db, 'convertToObjectId');
+        convertToObjectId.returns('123');
+
+        const req = {
+            body: {
+                accountId: '123'
+            }
+        }
+        
+        const res = {
+			status: sinon.stub().returnsThis(),
+			json: sinon.stub(),
+			send: sinon.stub(),
+		};
+
+        const filter = {
+            _id: convertToObjectId('123')
+        };
+
+        sinon.stub(db, 'updateOne').yields({});
+        accountController.postEditStatusPending(req, res);
+
+        assert.isTrue(db.updateOne.calledOnce);
+        assert.equal(db.updateOne.firstCall.args[0], Account);
+		expect(db.updateOne.firstCall.args[1]).to.deep.equalInAnyOrder(filter);
+		expect(db.updateOne.firstCall.args[2]).to.deep.equalInAnyOrder({status: 'Pending'});
+
+        db.convertToObjectId.restore();
+        db.updateOne.restore();
+    });
+});
+
+describe('the function to update the status of the selected user account to "Accepted"', function() {
+    it('should update the status of the account to "Accepted" only once', function() {
+        let convertToObjectId = sinon.stub(db, 'convertToObjectId');
+        convertToObjectId.returns('123');
+
+        const req = {
+            body: {
+                accountId: '123'
+            }
+        }
+        
+        const res = {
+			status: sinon.stub().returnsThis(),
+			json: sinon.stub(),
+			send: sinon.stub(),
+		};
+
+        const filter = {
+            _id: convertToObjectId('123')
+        };
+
+        sinon.stub(db, 'updateOne').yields({});
+        accountController.postEditStatusAccept(req, res);
+
+        assert.isTrue(db.updateOne.calledOnce);
+        assert.equal(db.updateOne.firstCall.args[0], Account);
+		expect(db.updateOne.firstCall.args[1]).to.deep.equalInAnyOrder(filter);
+		expect(db.updateOne.firstCall.args[2]).to.deep.equalInAnyOrder({status: 'Accepted'});
+
+        db.convertToObjectId.restore();
+        db.updateOne.restore();
+    });
+});
+
+describe('the function to update the role of the selected user account', function() {
+    it('should update the role of the account only once', function() {
+        let convertToObjectId = sinon.stub(db, 'convertToObjectId');
+        convertToObjectId.returns('123');
+
+        const req = {
+            body: {
+                accountId: '123',
+                editAccountRole: 'inventory-manager'
+            }
+        }
+        
+        const res = {
+			status: sinon.stub().returnsThis(),
+			json: sinon.stub(),
+			send: sinon.stub(),
+		};
+
+        const filter = {
+            _id: convertToObjectId('123')
+        };
+
+        sinon.stub(db, 'updateOne').yields({});
+        accountController.postEditRole(req, res);
+
+        assert.isTrue(db.updateOne.calledOnce);
+        assert.equal(db.updateOne.firstCall.args[0], Account);
+		expect(db.updateOne.firstCall.args[1]).to.deep.equalInAnyOrder(filter);
+		expect(db.updateOne.firstCall.args[2]).to.deep.equalInAnyOrder({role: req.body.editAccountRole});
+
+        db.convertToObjectId.restore();
+        db.updateOne.restore();
+    });
+});
+
+describe('the function to delete the selected user account', function() {
+    it('should delete the account only once', function() {
+        let convertToObjectId = sinon.stub(db, 'convertToObjectId');
+        convertToObjectId.returns('123');
+
+        const req = {
+            body: {
+                accountId: '123',
+            }
+        }
+        
+        const res = {
+			status: sinon.stub().returnsThis(),
+			json: sinon.stub(),
+			send: sinon.stub(),
+		};
+
+        const filter = {
+            _id: convertToObjectId('123')
+        };
+
+        sinon.stub(db, 'deleteOne').yields({});
+        accountController.postDeleteAccount(req, res);
+
+        assert.isTrue(db.deleteOne.calledOnce);
+        assert.equal(db.deleteOne.firstCall.args[0], Account);
+		expect(db.deleteOne.firstCall.args[1]).to.deep.equalInAnyOrder(filter);
+
+        db.convertToObjectId.restore();
+        db.deleteOne.restore();
     });
 });
