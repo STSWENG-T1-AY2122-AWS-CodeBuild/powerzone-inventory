6ce00e1090a15c94b87dd107b7731ca6f1b06ccf
Hylene Lee <hylene_jules_lee@dlsu.edu.ph>
2022-01-08 16:08:59 +0800

    Store transaction number as a database entry

---
+++ misc/populate-transaction-number.js
@@ -0,0 +1,21 @@
+/* Script for populating the database with the transaction number of the next transaction */
+
+/* The db file and transaction number schema are used to manipulate the transaction number on the database. */
+const db = require('../models/db.js');
+const TransactionNumber = require('../models/transaction-number-schema.js');
+
+/* Establish a connection to the database. */
+db.connect();
+
+/* Initialize the transaction number */
+const transactionNumber = {
+	label: 'nextTransaction',
+	transactionNumber: 2
+};
+
+/* Insert the transaction number into the database. */
+db.insertOne(TransactionNumber, transactionNumber, function(flag) {
+	if (flag) {
+		console.log('\nDatabase population complete! Press Ctrl + C to continue.');
+	}
+});
---
+++ models/transaction-number-schema.js
@@ -0,0 +1,20 @@
+/* Schema for the transaction number of the next transaction */
+
+/* Mongoose is used for database functions */
+const mongoose = require('mongoose');
+
+const transactionNumberSchema = new mongoose.Schema({
+    /* Label of the database entry for database retrieval */
+	label: {
+		type: String,
+		required: true
+	},
+
+	/* Number of the next transaction */
+	transactionNumber: {
+		type: Number,
+		required: true
+	}
+});
+
+module.exports = mongoose.model('TransactionNumber', transactionNumberSchema);
