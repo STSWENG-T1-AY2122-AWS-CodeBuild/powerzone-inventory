3ff3a39efb6a6352fa8fc7b0f2d47bb28f4ba96d
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2022-01-09 23:35:57 +0800

    Implement error checking if purchase exceeds reserves

--- public/js/edit-transaction.js
+++ public/js/edit-transaction.js
@@ -1,3 +1,14 @@
 $(function() {
     $('#edit-transaction-status').val($('#edit-transaction-status-hidden').val());
+
+    const fuelTypes = ['gasoline', 'premium-gasoline-95', 'diesel', 'premium-gasoline-97', 'kerosene'];
+
+    for (const fuelType of fuelTypes) {
+        $('#edit-transaction-' + fuelType + '-liters').on('keyup', function() {
+            if (parseInt($('#edit-transaction-' + fuelType + '-liters').val()) > 
+                parseInt($('#edit-transaction-' + fuelType + '-total').val())) {
+                alert(">:(");
+            }
+        })
+    }
 });
