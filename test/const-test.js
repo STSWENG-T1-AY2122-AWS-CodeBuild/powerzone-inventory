0763282a6ed2c512648da602d8c9868360bb030e
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2022-01-11 01:08:38 +0800

    Unit test filtering methods in transaction utility script

--- test/const-test.js
+++ test/const-test.js
@@ -16,6 +16,22 @@ const getDom = function() {
                             <tr id = "r8"><td>Diesel</td><td>01/07/2022</td><td></td><td>₱ 234</td></tr>
                         </tbody>
                     </table>
+                    <table id = "tbl2" style = "visibility: visible;">
+                        <tbody>
+                            <tr id = "s1">
+                                <td></td><td>01/01/2022</td><td>Shell</td><td>₱ 60</td>
+                                <td><img src = "/assets/pending.png"></td>
+                            </tr>
+                            <tr id = "s2">
+                                <td></td><td>01/01/2022</td><td>Chevron</td><td>₱ 120.34</td>
+                                <td><img src = "/assets/rejected.png"></td>
+                            </tr>
+                            <tr id = "s3">
+                                <td></td><td>01/03/2022</td><td>Petron</td><td>₱ 9.31</td>
+                                <td><img src = "/assets/accepted.png"></td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </body>
             </html>`;
 };
