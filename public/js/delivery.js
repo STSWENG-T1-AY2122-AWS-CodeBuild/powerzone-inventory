777e9acccf2a805334ee00deb11a3524c4e9bb7b
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2022-01-12 01:01:12 +0800

    Add initial code for filtering and ordering deliveries

--- public/js/delivery.js
+++ public/js/delivery.js
@@ -1,11 +1,15 @@
 /* JavaScript file for handling the front-end of the delivery page */
 
-import {getStatusFromIcon} from './transaction-util.js';
-
 import {extractId} from './general-util.js';
+import {
+	filterBy,
+	sortAtoZ,
+	sortZtoA,
+	getStatusFromIcon
+} from './transaction-util.js';
 
 $(function() {
-/* Update the details in the modal when the edit status button is clicked. */
+    /* Update the details in the modal when the edit status button is clicked. */
     $('.edit-delivery-status').on('click', function() {
         const transactionId = extractId($(this).attr('id'));
 
@@ -14,4 +18,38 @@ $(function() {
         $('#edit-delivery-status-form-display-id').text($('#id-' + transactionId).text());
         $('#edit-delivery-status-form-customer').text($('#customer-' + transactionId).text());
     });
+
+    /* Display the original transaction table upon reset of filters and sorting. */
+	const transactionTableId = 'transaction-table';
+	$('#reset-sort-transaction').on('click', function() {
+		$('input').val('');
+		$('input').prop('checked', false);
+
+		$('#' + transactionTableId).html($('#' + transactionTableId + '-orig').html());
+	});
+
+	/* Perform filtering and/or sorting of entries. */
+	$('input').on('change', function() {
+		/* Sort first before filtering. */
+		if ($('#sort-transaction-customer-name-az').is(':checked')) {
+			sortAtoZ(transactionTableId);
+		} else if ($('#sort-transaction-customer-name-za').is(':checked')) {
+			sortZtoA(transactionTableId);
+		} else if ($('#sort-transaction-price-lohi').is(':checked')) {
+			sortLowToHigh(transactionTableId);
+		} else if ($('#sort-transaction-price-hilo').is(':checked')) {
+			sortHighToLow(transactionTableId);
+		}
+
+		const statusTypes = ['pending', 'completed', 'cancelled'];
+		const selectedStatusTypes = [];
+
+		for (const statusType of statusTypes) {
+			if ($('#sort-transaction-' + statusType + '-status').is(':checked')) {
+				selectedStatusTypes.push(statusType);
+			}
+		}
+
+		filterBy(transactionTableId, selectedStatusTypes, $('#transaction-date').val());
+	});
 });
