c152646d59e6c266a4971df0eb10566c283d0e4d
Mark Edward M. Gonzales <gonzales.markedward@gmail.com>
2022-01-11 23:24:25 +0800

    Fix linter warnings

--- test/log-in-controller-util-test.js
+++ test/log-in-controller-util-test.js
@@ -9,59 +9,59 @@ const logInControllerUtil = require('../controllers/log-in-controller-util.js');
 const bcrypt = require('bcrypt');
 
 describe('the function to grant access depending on whether the password is equivalent to the hashed password in the database', function() {
-    let res;
-    let req;
-    let result;
+	let res;
+	let req;
+	let result;
 
-    beforeEach(function() {
-        res = {
-            status: sinon.stub().returnsThis(),
+	beforeEach(function() {
+		res = {
+			status: sinon.stub().returnsThis(),
 			json: sinon.stub(),
-            send: sinon.spy()
-        };
+			send: sinon.spy()
+		};
 
-        req = {
-            session: {
-                username: '',
-                role: ''
-            }
-        }
+		req = {
+			session: {
+				username: '',
+				role: ''
+			}
+		};
 
-        result = {
-            username: 'kikyo',
-            role: 'administrator'
-        };
-    });
+		result = {
+			username: 'kikyo',
+			role: 'administrator'
+		};
+	});
 
-    afterEach(function() {
-        bcrypt.compare.restore();
-    });
+	afterEach(function() {
+		bcrypt.compare.restore();
+	});
 
-    it('should set the session username to the account username if the password matches the value in the database', function() {
-        sinon.stub(bcrypt, 'compare').yields(null, true);
-        logInControllerUtil.logInUtil(req, res, result, "hello");
+	it('should set the session username to the account username if the password matches the value in the database', function() {
+		sinon.stub(bcrypt, 'compare').yields(null, true);
+		logInControllerUtil.logInUtil(req, res, result, 'hello');
 
-        assert.equal(req.session.username, result.username);
-    });
+		assert.equal(req.session.username, result.username);
+	});
 
-    it('should set the session role to the account role if the password matches the value in the database', function() {
-        sinon.stub(bcrypt, 'compare').yields(null, true);
-        logInControllerUtil.logInUtil(req, res, result, "hello");
+	it('should set the session role to the account role if the password matches the value in the database', function() {
+		sinon.stub(bcrypt, 'compare').yields(null, true);
+		logInControllerUtil.logInUtil(req, res, result, 'hello');
 
-        assert.equal(req.session.role, result.role);
-    });
+		assert.equal(req.session.role, result.role);
+	});
 
-    it('should not set the session username to the account username if the password does not match the value in the database', function() {
-        sinon.stub(bcrypt, 'compare').yields(null, false);
-        logInControllerUtil.logInUtil(req, res, result, "hello");
+	it('should not set the session username to the account username if the password does not match the value in the database', function() {
+		sinon.stub(bcrypt, 'compare').yields(null, false);
+		logInControllerUtil.logInUtil(req, res, result, 'hello');
 
-        assert.equal(req.session.username, '');
-    });
+		assert.equal(req.session.username, '');
+	});
 
-    it('should not set the session role to the account role if the password does not match the value in the database', function() {
-        sinon.stub(bcrypt, 'compare').yields(null, false);
-        logInControllerUtil.logInUtil(req, res, result, "hello");
+	it('should not set the session role to the account role if the password does not match the value in the database', function() {
+		sinon.stub(bcrypt, 'compare').yields(null, false);
+		logInControllerUtil.logInUtil(req, res, result, 'hello');
 
-        assert.equal(req.session.role, '');
-    });
+		assert.equal(req.session.role, '');
+	});
 });
